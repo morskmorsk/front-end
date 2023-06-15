e70ad7443500c802e4d89546f1afa9c06c8e3441
morsk <khalidfour@yahoo.com>
2023-06-15 23:57:26 +0100

    fix sidenav

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../services/customer.service';
+import { LoginService } from '../services/login.service';
+import { tap, catchError, switchMap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
@@ -8,18 +12,44 @@ import { CustomerService } from '../services/customer.service';
 export class CustomersComponent implements OnInit {
 
   customers:any = [];
+  refreshToken:any = localStorage.getItem('refresh_token')
 
-  constructor(public customerService: CustomerService) { }
+  constructor(public customerService: CustomerService, private loginService: LoginService) { }
 
   ngOnInit(): void {
-    this.customerService.getCustomers().subscribe(
-      response => {
-        this.customers = response;
-        console.log(this.customers);
-      },
-      error => {
-        console.log(error);
-      }
+    this.customerService.getCustomers().pipe(
+      tap(
+        response => {
+          this.customers = response;
+          console.log(this.customers);
+        },
+        error => {
+          this.handleError(error);
+        }
+      ),
+      catchError(error => {
+        if (error.status === 401) {
+          return this.refreshToken.pipe(
+            switchMap(() => {
+              return this.customerService.getCustomers();
+            })
+          );
+        } else {
+          throw error;
+        }
+      })
+    ).subscribe();
+  }
+
+  private refreshTokenAndRetry(request: Observable<any>): Observable<any> {
+    return this.refreshToken.pipe(
+      switchMap(() => {
+        return request;
+      })
     );
+  }
+
+  private handleError(error: any): void {
+    console.log(error);
   }
 }
